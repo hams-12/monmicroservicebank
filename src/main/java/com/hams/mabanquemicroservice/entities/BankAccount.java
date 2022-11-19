575c19753cc1b06642bd265a772532d7d8ee8142
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2022-11-19 15:46:52 +0100

    Ajout de la notation enumerated pour régler le problème d'affichage du champ type dans la bd

--- src/main/java/com/hams/mabanquemicroservice/entities/BankAccount.java
+++ src/main/java/com/hams/mabanquemicroservice/entities/BankAccount.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import java.util.Date;
 
@@ -17,5 +19,6 @@ public class BankAccount {
     private String id;
     private Date createdAt;
     private double balance;
+    @Enumerated(EnumType.STRING)
     private AccoutType type;
 }
