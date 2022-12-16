9df0a94588f886d26cc0ee853c6cafb5c2aaa177
Cheickna Hamala SANTARA <santarahamala@gmail.com>
2022-12-16 23:32:05 +0100

    éAjout d'un alias dans le repository pour le nom de la methode dans le repository

--- src/main/java/com/hams/mabanquemicroservice/repositories/BankAccountRepository.java
+++ src/main/java/com/hams/mabanquemicroservice/repositories/BankAccountRepository.java
@@ -3,11 +3,15 @@ package com.hams.mabanquemicroservice.repositories;
 import com.hams.mabanquemicroservice.entities.BankAccount;
 import com.hams.mabanquemicroservice.enums.AccoutType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
-    List<BankAccount> findByType(AccoutType type);
+    @RestResource(path = "/byType")
+    List<BankAccount> findByType(@Param("t") AccoutType type);
 }
